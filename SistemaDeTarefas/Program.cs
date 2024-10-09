efc17c642d7fc6503f091cc55a4de83e830d459a
Gesaias Alves de Souza <gesaiasalvessouza@gmail.com>
2024-10-09 17:21:36 -0400

    first commit

---
+++ SistemaDeTarefas/Program.cs
@@ -0,0 +1,49 @@
+using Microsoft.EntityFrameworkCore;
+using SistemaDeTarefas.Data;
+using SistemaDeTarefas.Repositories;
+using SistemaDeTarefas.Repositories.Interfaces;
+
+namespace SistemaDeTarefas
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            // Abrir conexão com o banco de dados
+            builder.Services
+                .AddDbContext<SistemaTarefasDBContext>(
+                    options => options.UseNpgsql(builder.Configuration.GetConnectionString("Database"))
+                );
+
+            // Injeção de dependências Repositories
+            builder.Services.AddScoped<IUsuarioRepository, UsuarioRepository>();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
---
+++ SistemaDeTarefas/Repositories/UsuarioRepository.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemaDeTarefas.Data;
+using SistemaDeTarefas.Models;
+using SistemaDeTarefas.Repositories.Interfaces;
+
+namespace SistemaDeTarefas.Repositories
+{
+    public class UsuarioRepository : IUsuarioRepository
+    {
+        private readonly SistemaTarefasDBContext _dbContext;
+
+        public UsuarioRepository(SistemaTarefasDBContext sistemaTarefasDBContext)
+        {
+            _dbContext = sistemaTarefasDBContext;
+        }
+        public async Task<UsuarioModel> BuscarPorId(int id) => await _dbContext.Usuarios.FirstOrDefaultAsync(x => x.Id == id);
+
+        public async Task<List<UsuarioModel>> BuscarTodosUsuarios()
+        {
+            return await _dbContext.Usuarios.ToListAsync();
+        }
+        public async Task<UsuarioModel> Adicionar(UsuarioModel usuario)
+        {
+            await _dbContext.Usuarios.AddAsync(usuario);
+            await _dbContext.SaveChangesAsync();
+
+            return usuario;
+        }
+
+        public async Task<bool> Apagar(int id)
+        {
+            UsuarioModel usuario = await BuscarPorId(id) ?? throw new Exception($"Usuário para o ID({id}) não foi encontrado no banco de dados!");
+
+            _dbContext.Usuarios.Remove(usuario);
+            await _dbContext.SaveChangesAsync();
+
+            return true;
+        }
+
+        public async Task<UsuarioModel> Atualizar(UsuarioModel usuario, int id)
+        {
+            UsuarioModel usuarioPorId = await BuscarPorId(id);
+
+            if (usuarioPorId == null)
+            {
+                throw new Exception($"Usuário para o ID({id}) não foi encontrado no banco de dados!");
+            }
+
+            usuarioPorId.Name = usuario.Name;
+            usuarioPorId.Email = usuario.Email;
+
+            _dbContext.Usuarios.Update(usuarioPorId);
+            await _dbContext.SaveChangesAsync();
+
+            return usuarioPorId;
+        }
+    }
+}
